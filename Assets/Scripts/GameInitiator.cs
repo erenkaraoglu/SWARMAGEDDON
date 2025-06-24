06bb69b666b39112f1098bd7b301e3e3b3bfc821
Gökhan Perçem <81044486+gpercem@users.noreply.github.com>
2025-06-24 11:14:49 +0300

    Game initiator update

--- Assets/Scripts/GameInitiator.cs
+++ Assets/Scripts/GameInitiator.cs
@@ -2,20 +2,21 @@ using UnityEngine;
 using Cysharp.Threading.Tasks;
 public class GameInitiator : MonoBehaviour
 {
-
-    [SerializeField] private LoadingScreen loadingScreen;
+    [Header("Prefab References")]
+    [SerializeField] private GameObject loadingScreen;
+    
+    [Header("Object References")]
+    [SerializeField] private Canvas canvas;
+    // Initialized references
+    private LoadingScreen _loadingScreen;
 
     private async void Start()
     {
-        loadingScreen.ShowImmediate();
-        await loadingScreen.SetProgressAsync(0.1f);
-        await UniTask.Delay(1000); // Simulate loading delay
-        await loadingScreen.SetProgressAsync(0.5f);
-        await UniTask.Delay(1000); // Simulate loading delay
-        await loadingScreen.SetProgressAsync(1f);
-        await UniTask.Delay(500); // Simulate final loading delay
-        await loadingScreen.HideAsync();
-        
+        _loadingScreen = Instantiate(loadingScreen, canvas.transform).GetComponent<LoadingScreen>();
+        _loadingScreen.ShowImmediate();
+        await _loadingScreen.SetProgressAsync(1f);
+        await _loadingScreen.HideAsync();
+
     }
     
 
